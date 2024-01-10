93e7edd2b086aebdc18d6e9ac1b6bf955e86d628
Aziza <aziza.saveleva@gmail.com>
2024-01-10 16:17:17 +0400

    Commit3, добавлен метод SelectRowsByLength

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿string[] SelectRowsByLength(string[] inletArray, int selectLength)
+{
+    int i = 0;
+    int k = 0;
+    while (i<inletArray.Length)
+    {
+        if (inletArray[i].Length<=selectLength)
+            k++;
+        i++;
+    }
+    string[] outletArray = new string[k];
+    i = 0;
+    int n = 0;
+    while (i<inletArray.Length)
+    {
+        if (inletArray[i].Length<=selectLength)
+        {
+            outletArray[n] = inletArray[i];
+            n++;
+        }
+        i++;
+    }
+    return outletArray;
+}
